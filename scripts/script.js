514d5a0eafa0d454a0b7fab829bffb9a99d801a6
yezzaab <115138980+yezzaab@users.noreply.github.com>
2025-11-26 23:50:28 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
 // JavaScript Document
 console.log("hi");
+
+//https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp code om de nav bar te laten wegvagen
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navheader").style.top = "0";
+  } else {
+    document.getElementById("navheader").style.top = "-30em";
+  }
+  prevScrollpos = currentScrollPos;
+}
